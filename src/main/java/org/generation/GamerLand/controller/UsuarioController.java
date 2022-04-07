0180c9650d24359124bff6ea60b7b30591e70ace
Igor Santos <igor.ox13@gmail.com>
2022-04-07 10:03:01 -0300

    atualização

--- src/main/java/org/generation/GamerLand/controller/UsuarioController.java
+++ src/main/java/org/generation/GamerLand/controller/UsuarioController.java
@@ -13,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -57,5 +59,10 @@ public class UsuarioController {
             .map(resp -> ResponseEntity.status(HttpStatus.OK).body(resp))
             .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
     }
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
 	
 }
